785fe31bf1f2a573692fd125167b7c240111b531
Sakshi13076 <sakshijadhav1307@gmail.com>
2026-02-13 16:31:24 +0530

    Server file updated'

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("EduScale Backend Running"); 
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("EduScale Backend is Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
+
